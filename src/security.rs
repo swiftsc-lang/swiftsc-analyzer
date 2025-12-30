fddaab01d9b6130e76999eac2c281fa6a0d3ae92
BronzonTech-Cloud <marvillomerveille3@gmail.com>
2025-12-30 18:06:33 +0000

    Prepare for v1.0.2-beta release

--- src/security.rs
+++ src/security.rs
@@ -1,5 +1,4 @@
-use crate::ast::*;
-use crate::sema::error::SemanticError;
+use swiftsc_frontend::ast::*;
 
 #[derive(Debug, Clone)]
 pub enum SecurityWarning {
@@ -12,23 +11,29 @@ pub struct SecurityAnalyzer {
     warnings: Vec<SecurityWarning>,
 }
 
+impl Default for SecurityAnalyzer {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl SecurityAnalyzer {
     pub fn new() -> Self {
         SecurityAnalyzer {
             warnings: Vec::new(),
         }
     }
-    
+
     pub fn analyze_program(&mut self, program: &Program) {
         for item in &program.items {
             match item {
                 Item::Contract(contract) => self.analyze_contract(contract),
                 Item::Function(func) => self.analyze_function(func),
-                Item::Dummy => {},
+                _ => {} // Ignore Use, Struct, Enum, etc. for now
             }
         }
     }
-    
+
     fn analyze_contract(&mut self, contract: &Contract) {
         for member in &contract.members {
             if let ContractMember::Function(func) = member {
@@ -38,17 +43,17 @@ impl SecurityAnalyzer {
             }
         }
     }
-    
+
     fn analyze_function(&mut self, func: &Function) {
         self.analyze_block(&func.body);
     }
-    
+
     fn analyze_block(&mut self, block: &Block) {
         for stmt in &block.stmts {
             self.analyze_statement(stmt);
         }
     }
-    
+
     fn analyze_statement(&mut self, stmt: &Statement) {
         match stmt {
             Statement::Let { init, .. } => {
@@ -63,7 +68,7 @@ impl SecurityAnalyzer {
             _ => {}
         }
     }
-    
+
     fn analyze_expression(&mut self, expr: &Expression) {
         match expr {
             Expression::Binary { left, op, right } => {
@@ -79,7 +84,7 @@ impl SecurityAnalyzer {
                 self.analyze_expression(left);
                 self.analyze_expression(right);
             }
-            Expression::Call { func, args } => {
+            Expression::Call { func, args, .. } => {
                 self.analyze_expression(func);
                 for arg in args {
                     self.analyze_expression(arg);
@@ -91,11 +96,11 @@ impl SecurityAnalyzer {
             _ => {}
         }
     }
-    
+
     pub fn get_warnings(&self) -> &[SecurityWarning] {
         &self.warnings
     }
-    
+
     pub fn has_critical_warnings(&self) -> bool {
         // For MVP, we don't have critical warnings yet
         false
